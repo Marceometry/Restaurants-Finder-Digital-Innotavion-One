3106545cbc312310c3a3ce64e221fc90adf1f2a1
Marceometry <marceometry@gmail.com>
2021-09-22 11:35:50 -0300

    add google maps

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { GoogleApiWrapper, Map as GoogleMap, Marker } from 'google-maps-react'
 import { MapContainer } from './styles'
 
-export function Map() {
-  return <MapContainer />
+export function Map({ google }) {
+  return (
+    <MapContainer>
+      <GoogleMap google={google} centerAroundCurrentLocation />
+    </MapContainer>
+  )
 }
+
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  language: 'pt-BR',
+})(Map)
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Aside } from '../../components/Aside'
-import { Map } from '../../components/Map'
+import Map from '../../components/Map'
 import { Wrapper } from './styles'
 
 export function Home() {
